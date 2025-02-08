cab30de5d2004de440fcb4a4c6516a953c10802e
edwinfa95 <edwinr95@hotmail.com>
2025-02-08 15:31:23 -0500

    Fix component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TodosComponent } from './todos/todos.component';
 import { Amplify } from 'aws-amplify';
-import outputs from '../../amplify_outputs.json';
+/*import outputs from '../../amplify_outputs.json';
 
-Amplify.configure(outputs);
+Amplify.configure(outputs);*/
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  imports: [RouterOutlet, TodosComponent],
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet],
 })
 export class AppComponent {
   title = 'barberty';
