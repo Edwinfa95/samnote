7af63d60d01eb4b28c4bb69a6d9721c4b92c9c33
edwinfa95 <edwinr95@hotmail.com>
2025-02-20 17:26:18 -0500

    Add lambda

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,10 +2,12 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { configApi } from './API/config';
+import { invocesApiFunction } from './functions/invoces/resource';
 
 const backend = defineBackend({
   auth,
   data,
+  invocesApiFunction
 });
 
 configApi(backend);
